f233736b230978c073c93f82ccfd5618ec2449b1
Kiree Bellamy <kireebellamy@gmail.com>
2024-05-18 15:24:24 +0000

    Added background image and styled it

--- werecat/settings.py
+++ werecat/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-swewi-werecatblog-2nhm2yjy91d.ws-eu111.gitpod.io',
+ALLOWED_HOSTS = ['8000-swewi-werecatblog-7tdy8qpovj1.ws-eu111.gitpod.io',
                 '.herokuapp.com'
                 ]
 
