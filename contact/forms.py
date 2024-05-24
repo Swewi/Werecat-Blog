772f97662e7d806297f9ce63d0414acf78f9c699
Kiree Bellamy <kireebellamy@gmail.com>
2024-05-24 14:19:37 +0000

    added images for readme

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,8 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('name', 'email', 'message')
+        labels = {
+            'name': 'Your Name',
+            'email': 'Your Email',
+            'message': 'Your Message'
+        }
