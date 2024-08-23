f8df4ebf6b3a11ee12c334dd89351b0af69c1ae0
Kiree Bellamy <kireebellamy@gmail.com>
2024-08-23 12:45:41 +0000

    fixed display issues in several screen sizes

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        label="Full Name",
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Please enter full name here',
+            'aria-label': 'Full Name'
+        })
+    )
+    email = forms.EmailField(
+        label="Email Address",
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Please enter a valid email here',
+            'aria-label': 'Email Address'
+        })
+    )
+    message = forms.CharField(
+        label="Message",
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Please enter your message here',
+            'aria-label': 'Message'
+        })
+    )
