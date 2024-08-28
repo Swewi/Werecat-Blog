8b7dcf499dc948322ab5da141c1272bd5fa1cc73
Kiree Bellamy <kireebellamy@gmail.com>
2024-08-28 14:24:46 +0000

    Added 400, 403, 404, and 500 pages

--- werecat/settings.py
+++ werecat/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = False   # False for production
 
 # Define allowed hosts for the application
 
